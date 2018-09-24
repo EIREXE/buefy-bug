a4f84b10ccfdc0f6c8e3a582e35410fad3838467
Alex Roman <eirexe123@gmail.com>
2018-09-24 23:47:00 +0200

    Add failing test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,17 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+
+import Vue from 'vue'
+import Buefy from 'buefy'
+
+Vue.use(Buefy)
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = mount(HelloWorld, {
       propsData: { msg }
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.find({ ref: 'a1b' }))
   })
 })
